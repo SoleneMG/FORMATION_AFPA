ac56c1ae700b190cff122f677bd81708517abc95
SoleneMG <solene.michelgrosjean@gmail.com>
2021-10-29 10:21:12 +0200

    tp property tp copy

---
+++ ANT/build.bat
@@ -0,0 +1,17 @@
+@echo off
+d:
+cd TP_ANT
+copy con r1.zip
+:: write some text and click on ctrl Z and after enter
+mkdir r1
+cd r1
+type nul > tata
+type nul > ab12
+type nul > tutu
+type nul > titi
+type nul > toto
+type nul > trivial
+type nul > t632
+type nul > tbc2
+type nul > tnt
+type nul > t
