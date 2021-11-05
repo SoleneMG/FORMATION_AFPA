02b0e5e6efe3ea1e78e446b67455d60b77d598d1
SoleneMG <solene.michelgrosjean@gmail.com>
2021-11-05 15:06:41 +0100

    Add 3 favorites horses

--- JAVA/Exercice03/Exercice03.java
+++ JAVA/Exercice03/Exercice03.java
@@ -1,3 +1,4 @@
+package Exercice03;
 public class Exercice03 {
     
     public static void main(String[] args){
@@ -7,6 +8,7 @@ public class Exercice03 {
         String adress;
         int numberOfFNTECard;
         int numberOfFavoriteHorses = 3;
+        String singularWordHorse = "horse";
         Horse horse;
         char again;
         
@@ -16,33 +18,63 @@ public class Exercice03 {
             adress = sc.returnStringWithoutSpecialCharacters("What is your adress ? ");
             numberOfFNTECard = sc.returnInt("What is your number of you're FNTE card ? ");
 
+            Horse[] horses = new Horse[numberOfFavoriteHorses];
+            
             for (int start=1; start<=numberOfFNTECard; start++){
-                String extensionOfNumber;
+                String extensionOfNumber="st";
                 String numberIncrement = String.valueOf(start);
-                
-                    if(numberIncrement.matches("*[1]$")){
-                        extensionOfNumber = "st";
-                    }
-                    if(numberIncrement.matches("*[2]$")){
-                        extensionOfNumber = "nd";
-                    }
-                    if(numberIncrement.matches("*[3]$")){
-                        extensionOfNumber = "rd";
-                    }
-                    if( !(numberIncrement.equals(0)) ){
+
+
+                ////////////////////////////////////////////////////////
+                // extension 1st 2nd 3rd 4th ....
+                if(numberIncrement.matches("*[1]$")){
+                    extensionOfNumber = "st";
+                }
+                if(numberIncrement.matches("*[2]$")){
+                    extensionOfNumber = "nd";
+                }
+                if(numberIncrement.matches("*[3]$")){
+                    extensionOfNumber = "rd";
+                }
+                if( !(numberIncrement.equals(0)) ){
                     extensionOfNumber="th";
                 }
-               
-                String horseName = sc.returnStringWithoutSpecialCharactersAndNumbers(String.format("What is name of the name of the %d%s horse ?", start, numberIncrement));
-                horse = new Horse(name)
-            }
+                ///////////////////////////////////////////////////////
 
-            Rider rider = new Rider(name, firstName);
-            System.out.printf("Rider's name is : %s and his firstname is : %s.", rider.name, rider.firstName +".\n");
-            
+                ///////////////////////////////////////////////////////
+                // use typo horse or horses 
+                if(start>1) singularWordHorse = "horses";
+                /////////////////////////////////////////////////////
+
+                String horseName = sc.returnStringWithoutSpecialCharactersAndNumbers(String.format("What is name of the name of the %d%s horse ?", start, extensionOfNumber));
+                horse = new Horse(horseName);
+                horses[start-1]=horse;
+            }
+        
+            Rider rider = new Rider(name, firstName, adress, numberOfFNTECard, horses);
+            System.out.printf("Rider's name is : %s. His firstname is : %s.\n", rider.name, rider.firstName);
+            System.out.printf("His adress is %s, his number of FNTE card is %d. \n", rider.adress, rider.numberOfFNTECard);
+            System.out.printf("He has %d favorite %s. \n", numberOfFavoriteHorses, singularWordHorse);
+
+            if(horses.length!=0){
+                if(horses.length>1){
+                    System.out.printf("Their names are : ");
+                } else {
+                    System.out.printf("His name is : ");
+                }
+                for(int i=0; i<horses.length;i++){
+                    System.out.print(horses[i].name);
+                    if(i==horses.length){
+                        System.out.print(". \n");
+                    }else {
+                        System.out.print(", ");
+                    }
+                }
+            }
+           
             again = sc.returnChar("Do you want continue ? Please enter y (yes) or n (no) : ");
         } while(again=='y');
-
+        
         sc.close();
         
     }
--- JAVA/Exercice03/Rider.java
+++ JAVA/Exercice03/Rider.java
@@ -1,10 +1,17 @@
+package Exercice03;
 public class Rider {
     public final String name;
     public final String firstName;
+    public final String adress;
+    public final int numberOfFNTECard;
+    public final Horse[] horses;
 
-    public Rider(String name, String firstName){
+    public Rider(String name, String firstName, String adress, int numberOfFNTECard, Horse[] horses){
         this.name = name;
         this.firstName = firstName;
+        this.adress = adress;
+        this.numberOfFNTECard = numberOfFNTECard;
+        this.horses = horses;
     }
     
 }
