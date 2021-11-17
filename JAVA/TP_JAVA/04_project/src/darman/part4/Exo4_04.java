65d3875c68a5b99937955c84aed135f179d28928
77011-05-07 <77011-05-07@exchange.ad.afpanet>
2021-11-17 13:36:27 +0100

    darmangeat part 4 exercice 4

--- JAVA/TP_JAVA/04_project/src/darman/part4/Exo4_04.java
+++ JAVA/TP_JAVA/04_project/src/darman/part4/Exo4_04.java
@@ -1,10 +1,50 @@
 package darman.part4;
 
+import java.text.DecimalFormat;
+import java.text.Format;
+import java.util.Scanner;
+
 public class Exo4_04 {
+	public static Scanner sc = new Scanner(System.in);
+
+	/**
+	 * Un magasin de reprographie facture 0,10 E les dix premières photocopies, 0,09
+	 * E les vingt suivantes et 0,08 E au-delà. Ecrivez un algorithme qui demande à
+	 * l’utilisateur le nombre de photocopies effectuées et qui affiche la facture
+	 * correspondante.
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int quantity;
+		double price;
+		Format format = new DecimalFormat("0.00");
+		
+		do {
+		System.out.println("Combien de photocopies désirez-vous effectuer ? ");
+		quantity = sc.nextInt();
+		sc.nextLine();
+		} while (quantity <0 || quantity == 0);
 
+		if(quantity <11) {
+			price = 0.10;
+			String priceFormatted = format.format(calculatePrice(quantity, price));
+			System.out.printf("Pour %d copie(s), votre facture s'élève à %s €",quantity , priceFormatted );
+		} else if(quantity<21) {
+			price = 0.09;
+			String priceFormatted = format.format(calculatePrice(quantity, price));
+			System.out.printf("Pour %d copie(s), votre facture s'élève à %s €",quantity , priceFormatted );
+		}else {
+			price = 0.08;
+			String priceFormatted = format.format(calculatePrice(quantity, price));
+			System.out.printf("Pour %d copie(s), votre facture s'élève à %s €",quantity , priceFormatted );
+		}
+
+		sc.close();
+
+	}
+
+	public static double calculatePrice(int quantity, double price) {
+		return quantity * price;
 	}
 
 }
