74fa304c960d2969db05feed2cae11b13fd43ea7
SoleneMG <solene.michelgrosjean@gmail.com>
2021-11-18 10:01:54 +0100

    add darmangeat Exo05_03

---
+++ JAVA/TP_JAVA/04_project/src/darman/part5/Exo5_03.java
@@ -0,0 +1,29 @@
+package darman.part5;
+
+import java.util.Scanner;
+
+public class Exo5_03 {
+	public static Scanner sc = new Scanner(System.in);
+
+	/**
+	 * Ecrire un algorithme qui demande un nombre de départ, et qui ensuite affiche
+	 * les dix nombres suivants. Par exemple, si l'utilisateur entre le nombre 17,
+	 * le programme affichera les nombres de 18 à 27.
+	 */
+
+	public static void main(String[] args) {
+
+		System.out.println("Entrer un nombre : ");
+		int number = sc.nextInt();
+		sc.nextLine();
+
+		System.out.printf("Vous avez saisi le nombre %d",number);
+		System.out.println("\nVoici les dix nombres suivants :");
+		for (int i = 1; i < 11; i++) {
+			System.out.println(number + i);
+		}
+
+		sc.close();
+	}
+
+}
