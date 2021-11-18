2ed0110159b889aa8767f6f91270ffeb8e75ed52
SoleneMG <solene.michelgrosjean@gmail.com>
2021-11-18 23:17:01 +0100

    add darmangeat Exo06_08

---
+++ JAVA/TP_JAVA/04_project/src/darman/part6/Exo6_08.java
@@ -0,0 +1,53 @@
+package darman.part6;
+
+import java.text.DecimalFormat;
+import java.text.Format;
+import java.util.Scanner;
+
+public class Exo6_08 {
+	public static Scanner sc = new Scanner(System.in);
+
+	/**
+	 * EEcrivez un algorithme permettant à l’utilisateur de saisir un nombre
+	 * quelconque de valeurs, qui devront être stockées dans un tableau.
+	 * L’utilisateur doit donc commencer par entrer le nombre de valeurs qu’il
+	 * compte saisir. Il effectuera ensuite cette saisie. Enfin, une fois la saisie
+	 * terminée, le programme affichera le nombre de valeurs négatives et le nombre
+	 * de valeurs positives.
+	 */
+
+	public static void main(String[] args) {
+		
+		System.out.println("Entrer un nombre max de valeurs : ");
+		int length = sc.nextInt();
+		sc.nextLine();
+		
+		int[] board = new int[length];
+		int i = 0;
+		do {
+			System.out.printf("Saisir la valeur à l'index %d : \n", i);
+			board[i] = sc.nextInt();
+			sc.nextLine();
+			i++;
+		} while (i < length);
+		
+		int nbValeursNegatives=0;
+		int nbValeursPositives=0;
+		int nbValeursEgalesAZero = 0;
+		for (int x = 0; x < board.length; x++) {
+			if(board[x]<0) {
+				nbValeursNegatives++;
+			} else if(board[x]>0) {
+				nbValeursPositives++;
+			} else if(board[x]==0) {
+				nbValeursEgalesAZero++;
+			} else {
+				throw new IllegalArgumentException();
+			}
+		}
+	
+		System.out.printf("Nombres de valeurs positives : %d\n Nombres de valeurs négatives : %d\n", nbValeursPositives, nbValeursNegatives);
+
+	}
+
+}
