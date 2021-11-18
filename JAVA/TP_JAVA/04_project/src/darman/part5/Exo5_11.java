d547034537d754a2bd82b593f986c028dcc5deed
SoleneMG <solene.michelgrosjean@gmail.com>
2021-11-18 16:53:12 +0100

    add darmangeat Exo05 fix bug

--- JAVA/TP_JAVA/04_project/src/darman/part5/Exo5_11.java
+++ JAVA/TP_JAVA/04_project/src/darman/part5/Exo5_11.java
@@ -4,93 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.management.NotCompliantMBeanException;
+
 public class Exo5_11 {
 	public static Scanner sc = new Scanner(System.in);
 
 	/**
-	 * Lire la suite des prix (en euros entiers et terminée par zéro) des achats
-	 * d’un client. Calculer la somme qu’il doit, lire la somme qu’il paye, et
-	 * simuler la remise de la monnaie en affichant les textes "10 Euros", "5 Euros"
-	 * et "1 Euro" autant de fois qu’il y a de coupures de chaque sorte à rendre.
+	 * Écrire un algorithme qui permette de connaître ses chances de gagner au
+	 * tiercé, quarté, quinté et autres impôts volontaires. 
+	 * On demande à l’utilisateur le nombre de chevaux partants, et le nombre de chevaux joués.
+	 * Les deux messages affichés devront être : 
+	 * Dans l’ordre : une chance sur X de gagner 
+	 * Dans le désordre : une chance sur Y de gagner 
+	 * X et Y nous sont donnés par la formule suivante, si n est le nombre de chevaux partants 
+	 * et p le nombre de chevaux joués (on rappelle que le signe ! signifie "factorielle",
+	 * comme dans l'exercice 5.7 ci-dessus) : 
+	 * X = n ! / (n - p) ! 
+	 * Y = n ! / (p ! * (n – p) !) 
+	 * NB : cet algorithme peut être écrit d’une manière simple, mais
+	 * relativement peu performante. Ses performances peuvent être singulièrement
+	 * augmentées par une petite astuce. Vous commencerez par écrire la manière la
+	 * plus simple, puis vous identifierez le problème, et écrirez une deuxième
+	 * version permettant de le résoudre.
 	 */
 
 	public static void main(String[] args) {
-
-		List<Integer> achatListe = saisieArticle();
-		int total = calculPrixTotal(achatListe);
-		System.out.println("Le montant total de vos articles s'élèvent à " + total + "€");
-		int montantPaye;
-		do {
-		System.out.println("Saisir le montant remis par le client :");
-		montantPaye = sc.nextInt();
+		
+		System.out.println("Nombre de chevaux partants :");
+		int nbPartants = sc.nextInt();
 		sc.nextLine();
-		if(montantPaye < total) {
-			System.out.println("Le montant total des achats est supérieur");
-		}
-		}while (montantPaye < total);
-		renduMonnaie(total, montantPaye);
-
+		System.out.println("Nombre de chevaux joués :");
+		int nbJoues = sc.nextInt();
+		sc.nextLine();
+		
+		
+		
+		long chanceDeGagnerDansLOrdre =  calculFactorielle(nbPartants) / calculFactorielle(nbPartants - nbJoues);
+		long chanceDeGagnerDansLeDesordre = calculFactorielle(nbPartants) / ( calculFactorielle(nbJoues) * (calculFactorielle(nbPartants - nbJoues)));
+		
+		System.out.printf("Dans l'ordre : une chance sur %d de gagner\n", chanceDeGagnerDansLOrdre);
+		System.out.printf("Dans le désordre : une chance sur %d de gagner\n", chanceDeGagnerDansLeDesordre);
+		
 		sc.close();
 	}
 
-	public static List<Integer> saisieArticle() {
-		int i = 1;
-		int montant = -1;
-		List<Integer> achatListe = new ArrayList<Integer>();
-		do {
-			System.out.println("Prix article " + i + " :");
-			montant = sc.nextInt();
-			sc.nextLine();
-			achatListe.add(montant);
-			i++;
-			if (montant != 0) {
-				System.out.println("(Pour terminer la saisie entrer 0)");
-			}
-		} while (!(montant == 0));
-		return achatListe;
+	public static long calculFactorielle(int number) {
+		long result =1;
+		for (int i =1; i<=number; i++) {
+			result *=i;
+		}
+		return result;
 	}
 
-	public static int calculPrixTotal(List<Integer> achatListe) {
-		int total = 0;
-		for (int i = 0; i < achatListe.size(); i++) {
-			total += achatListe.get(i);
-		}
-		return total;
-	}
-
-	public static void renduMonnaie(int total, int paiement) {
-		int dixEuros = 0;
-		int cinqEuros = 0;
-		int unEuros = 0;
-
-		int renduMonnaie = paiement - total;
-		if (renduMonnaie >= 10) {
-			do {
-				dixEuros++;
-				renduMonnaie -= 10;
-			} while (renduMonnaie > 10);
-		}
-		if (renduMonnaie >= 5) {
-			do {
-				cinqEuros++;
-				renduMonnaie -= 5;
-			} while (renduMonnaie > 5);
-		}
-		if (renduMonnaie >= 1) {
-			do {
-				unEuros++;
-				renduMonnaie -= 1;
-			} while (renduMonnaie != 0);
-		}
-		if (dixEuros != 0) {
-			System.out.printf("Vous devez rendre au client %d x 10€\n", dixEuros);
-		}
-		if (cinqEuros != 0) {
-			System.out.printf("Vous devez rendre au client %d x 5€\n", cinqEuros);
-		}
-		if (unEuros != 0) {
-			System.out.printf("Vous devez rendre au client %d x 1€\n", unEuros);
-		}
-
-	}
 }
---
+++ JAVA/TP_JAVA/04_project/src/darman/part6/Exo6_01.java
@@ -0,0 +1,25 @@
+package darman.part6;
+
+import java.util.Scanner;
+
+public class Exo6_01 {
+	public static Scanner sc = new Scanner(System.in);
+
+	/**
+	 * Ecrire un algorithme qui demande à l’utilisateur un nombre compris entre 1 et
+	 * 3 jusqu’à ce que la réponse convienne.
+	 */
+
+	public static void main(String[] args) {
+		int number = 0;
+		do {
+			System.out.println("Entrer un nombre entre 1 et 3 : ");
+			number = sc.nextInt();
+			sc.nextLine();
+
+		} while ((number < 1 || number > 3));
+		System.out.printf("Super ! Vous avez choisi le %d", number);
+		sc.close();
+	}
+
+}
