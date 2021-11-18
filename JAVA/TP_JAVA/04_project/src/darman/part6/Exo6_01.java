dc6254ac9c9d02f99ff1623cbebea6ca9252a326
SoleneMG <solene.michelgrosjean@gmail.com>
2021-11-18 16:56:49 +0100

    add darmangeat Exo06_01

--- JAVA/TP_JAVA/04_project/src/darman/part6/Exo6_01.java
+++ JAVA/TP_JAVA/04_project/src/darman/part6/Exo6_01.java
@@ -1,25 +1,19 @@
 package darman.part6;
 
-import java.util.Scanner;
+
 
 public class Exo6_01 {
-	public static Scanner sc = new Scanner(System.in);
+
 
 	/**
-	 * Ecrire un algorithme qui demande à l’utilisateur un nombre compris entre 1 et
-	 * 3 jusqu’à ce que la réponse convienne.
+	 * Ecrire un algorithme qui déclare et remplisse un tableau de 7 valeurs
+	 * numériques en les mettant toutes à zéro.
 	 */
 
 	public static void main(String[] args) {
-		int number = 0;
-		do {
-			System.out.println("Entrer un nombre entre 1 et 3 : ");
-			number = sc.nextInt();
-			sc.nextLine();
-
-		} while ((number < 1 || number > 3));
-		System.out.printf("Super ! Vous avez choisi le %d", number);
-		sc.close();
+		
+		int[] board = {0,0,0,0,0,0,0};	
+		System.out.printf("Il y a %d valeurs dans le tableau.", board.length);
 	}
 
 }
