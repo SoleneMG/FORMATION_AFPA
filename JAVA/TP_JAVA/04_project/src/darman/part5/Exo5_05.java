bd80cf57f5e2d53bddf072132d26e133096989b9
SoleneMG <solene.michelgrosjean@gmail.com>
2021-11-18 10:10:06 +0100

    add darmangeat Exo05_05

--- JAVA/TP_JAVA/04_project/src/darman/part5/Exo5_05.java
+++ JAVA/TP_JAVA/04_project/src/darman/part5/Exo5_05.java
@@ -6,21 +6,21 @@ public class Exo5_05 {
 	public static Scanner sc = new Scanner(System.in);
 
 	/**
-	 * Réécrire l'algorithme précédent, en utilisant cette fois l'instruction While
+	 * Ecrire un algorithme qui demande un nombre de départ, et qui ensuite écrit la
+	 * table de multiplication de ce nombre, présentée comme suit (cas où
+	 * l'utilisateur entre le nombre 7) :
 	 */
 
 	public static void main(String[] args) {
-		int i = 1;
 
 		System.out.println("Entrer un nombre : ");
 		int number = sc.nextInt();
 		sc.nextLine();
 
 		System.out.printf("Vous avez saisi le nombre %d", number);
-		System.out.println("\nVoici les dix nombres suivants :");
-		while (i < 11) {
-			System.out.println(number + i);
-			i++;
+		System.out.println("\nVoici sa table de multiplication :");
+		for (int i = 0; i < 11; i++) {
+			System.out.println(number + " * " + i + " = " + number * i);
 		}
 
 		sc.close();
