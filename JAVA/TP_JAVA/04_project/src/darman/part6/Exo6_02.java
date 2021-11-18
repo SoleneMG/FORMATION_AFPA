9caafe51ea742db7c9d85a9760e2d3a541675404
SoleneMG <solene.michelgrosjean@gmail.com>
2021-11-18 16:58:36 +0100

    add darmangeat Exo06_02

---
+++ JAVA/TP_JAVA/04_project/src/darman/part6/Exo6_02.java
@@ -0,0 +1,18 @@
+package darman.part6;
+
+
+
+public class Exo6_02 {
+
+
+	/**
+	 * Ecrire un algorithme qui déclare et remplisse un tableau contenant les six voyelles de l’alphabet latin.
+	 */
+
+	public static void main(String[] args) {
+		
+		int[] board = {'a','e','i','o','u','y'};	
+		System.out.printf("Il y a %d valeurs dans le tableau.", board.length);
+	}
+
+}
