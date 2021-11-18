2eef25c18551c7280b8ac0e3185f5b3914fddb8a
SoleneMG <solene.michelgrosjean@gmail.com>
2021-11-18 09:55:12 +0100

    add format darmangeat Exo05_02

---
+++ JAVA/TP_JAVA/04_project/src/darman/part5/Exo5_02.java
@@ -0,0 +1,32 @@
+package darman.part5;
+
+import java.util.Scanner;
+
+public class Exo5_02 {
+	public static Scanner sc = new Scanner(System.in);
+
+	/**
+	 * Ecrire un algorithme qui demande un nombre compris entre 10 et 20, jusqu’à ce
+	 * que la réponse convienne. En cas de réponse supérieure à 20, on fera
+	 * apparaître un message : « Plus petit ! », et inversement, « Plus grand ! » si
+	 * le nombre est inférieur à 10.
+	 */
+
+	public static void main(String[] args) {
+		int number = 0;
+		do {
+			System.out.println("Entrer un nombre entre 10 et 20 : ");
+			number = sc.nextInt();
+			sc.nextLine();
+			if (number < 10) {
+				System.out.println("Il faut entrer un nombre plus grand !");
+			} else if (number > 20) {
+				System.out.println("Il faut entrer un nombre plus petit !");
+			}
+
+		} while ((number < 10 || number > 20));
+		System.out.printf("Super ! Vous avez choisi le chiffre %d", number);
+		sc.close();
+	}
+
+}
