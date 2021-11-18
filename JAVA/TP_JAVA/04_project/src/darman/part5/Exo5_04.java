70f95da468d228ca932210663a4aae4156f29c40
SoleneMG <solene.michelgrosjean@gmail.com>
2021-11-18 10:06:52 +0100

    add darmangeat Exo05_04

---
+++ JAVA/TP_JAVA/04_project/src/darman/part5/Exo5_04.java
@@ -0,0 +1,29 @@
+package darman.part5;
+
+import java.util.Scanner;
+
+public class Exo5_04 {
+	public static Scanner sc = new Scanner(System.in);
+
+	/**
+	 * Réécrire l'algorithme précédent, en utilisant cette fois l'instruction While
+	 */
+
+	public static void main(String[] args) {
+		int i = 1;
+
+		System.out.println("Entrer un nombre : ");
+		int number = sc.nextInt();
+		sc.nextLine();
+
+		System.out.printf("Vous avez saisi le nombre %d", number);
+		System.out.println("\nVoici les dix nombres suivants :");
+		while (i < 11) {
+			System.out.println(number + i);
+			i++;
+		}
+
+		sc.close();
+	}
+
+}
