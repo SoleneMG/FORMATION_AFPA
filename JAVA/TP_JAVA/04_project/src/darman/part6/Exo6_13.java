696c08136ee6a6dcdf5a183dc3cf151ffd988206
SoleneMG <solene.michelgrosjean@gmail.com>
2021-11-19 15:04:23 +0100

    part6/Exo6_13

--- JAVA/TP_JAVA/04_project/src/darman/part6/Exo6_13.java
+++ JAVA/TP_JAVA/04_project/src/darman/part6/Exo6_13.java
@@ -8,59 +8,44 @@ public class Exo6_13 {
 	public static Scanner sc = new Scanner(System.in);
 
 	/**
-	 * Ecrivez un algorithme permettant, toujours sur le même principe, à
-	 * l’utilisateur de saisir un nombre déterminé de valeurs. Le programme, une
-	 * fois la saisie terminée, renvoie la plus grande valeur en précisant quelle
-	 * position elle occupe dans le tableau. On prendra soin d’effectuer la saisie
-	 * dans un premier temps, et la recherche de la plus grande valeur du tableau
-	 * dans un second temps.
+	 * Toujours et encore sur le même principe, écrivez un algorithme permettant, à
+	 * l’utilisateur de saisir les notes d'une classe. Le programme, une fois la
+	 * saisie terminée, renvoie le nombre de ces notes supérieures à la moyenne de
+	 * la classe.
 	 */
 
 	public static void main(String[] args) {
-		int lengthBoard;
-		int i = 1;
-		int max = -1;
-		int position = 0;
-		int number = -1;
-		int[] positionBoard = new int[500];
+		int nbDeNotes;
+		int i = 1; // i incrémenté à 1 pour affichage note 1, 2 etc...
+		int totalDesNotes = 0;
+		int moyenne = 0;
+		int nbNotesAuDessusMoyenne = 0;
 
-		System.out.println("Entrer un nombre max de valeurs : ");
-		lengthBoard = sc.nextInt();
+		System.out.println("Combien de notes souhaitez-vous rentrer ? ");
+		nbDeNotes = sc.nextInt();
 		sc.nextLine();
-		int[] numberBoard = new int[lengthBoard];
+		int[] tableauDeNotes = new int[nbDeNotes];
 
-			do {
-			System.out.println("Entrer un nombre :");
-			number = sc.nextInt();
+		do {
+			System.out.printf("Note %d : ", i);
+			tableauDeNotes[i - 1] = sc.nextInt(); // i-1 car l'index ne démarre pas à 0 par soucis d'affichage
 			sc.nextLine();
-			
-			if (i == 1) {
-				max = number;
-				positionBoard[position] = i;
-			}
-			if (number >= max && i !=1) {
-				if (number == max) {
-					position++;
-				}
-				max = number;
-				positionBoard[position] = i;
-			}
-			i++;
-		} while (i < numberBoard.length+1);
 
-		if (position == 0) {
-			System.out.println("Le plus grand de ces nombres est : " + max + " \nC'était le nombre numéro " + positionBoard[position]);
-		} else {
-			System.out.println(
-					"Le plus grand de ces nombres est : " + max + " \nIl a été saisi plusieurs fois aux positions :");
-			for (int x = 0; x <= position; x++) {
-				if (!(x == position)) {
-					System.out.print(positionBoard[x]+ ", ");
-				} else {
-				System.out.print(positionBoard[x] );
-				}
+			totalDesNotes += tableauDeNotes[i - 1];
+			i++;
+		} while (i < tableauDeNotes.length + 1); // length+1 car l'index ne démarre pas à 0 par soucis d'affichage
+
+		moyenne = totalDesNotes / nbDeNotes;
+
+		for (int j = 0; j < tableauDeNotes.length; j++) {
+			if (tableauDeNotes[j] > moyenne) {
+				nbNotesAuDessusMoyenne++;
 			}
 		}
+
+		System.out.printf("La moyenne de la classe est de %d, %d notes sont au dessus", moyenne,
+				nbNotesAuDessusMoyenne);
+
 		sc.close();
 	}
 }
