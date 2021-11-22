b9d43d8f87610de8d9fcb8ebb7f3be217cdbdbdc
77011-05-07 <77011-05-07@exchange.ad.afpanet>
2021-11-22 15:31:07 +0100

    Exo8_01

--- JAVA/TP_JAVA/04_project/src/darman/part8/Exo8_01.java
+++ JAVA/TP_JAVA/04_project/src/darman/part8/Exo8_01.java
@@ -1,9 +1,20 @@
 package darman.part8;
 
+import java.util.Arrays;
+
 public class Exo8_01 {
+	
+	/** Écrivez un algorithme remplissant un tableau de 6 sur 13, avec des zéros. */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 int[][] board = new int[6][13];
+	 
+	 for (int[] lignes : board) {
+		System.out.println("");
+		for (int colonnes : lignes) {
+		System.out.print(lignes[colonnes]+" ");
+		}
+	}
 
 	}
 
