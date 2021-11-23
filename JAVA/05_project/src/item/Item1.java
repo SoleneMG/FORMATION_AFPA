796176ef50a2b3923962ff2794f999145df86a88
77011-05-07 <77011-05-07@exchange.ad.afpanet>
2021-11-23 13:14:51 +0100

    project 05 item1 et item2

---
+++ JAVA/05_project/src/item/Item1.java
@@ -0,0 +1,43 @@
+package item;
+
+import java.util.Scanner;
+
+public class Item1 {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		float indiceConversionEuroToDollar = 1.13f;
+		String devise = "$";
+		Item1 ex01 = new Item1();
+		long amount = ex01.askAnAmountInEuro(sc);
+
+		ex01.conversionDevise(indiceConversionEuroToDollar, amount, devise);
+		sc.close();
+	}
+
+	public long askAnAmountInEuro(Scanner sc) {
+		System.out.print("Saisissez un montant : ");
+		long result =  sc.nextLong();
+		sc.nextLine();
+		return result;
+	}
+
+	public void conversionDevise(float indiceConversion, long amount, String devise) {
+		char unitOfMeasurement = ' ';
+		double result;
+		double amountAfterConversion = indiceConversion * amount;
+
+		if (amountAfterConversion > 1_000 && amountAfterConversion < 1_000_000) {
+			result = amountAfterConversion / 1_000;
+			unitOfMeasurement = 'K';
+		} else if (amountAfterConversion > 1_000_000) {
+			result = amountAfterConversion / 1_000_000;
+			unitOfMeasurement = 'M';
+		} else {
+			result = amountAfterConversion;
+			// not a unitOfMeasurement
+		}
+		System.out.printf("Equivaut à %.2f%s %s",  result, unitOfMeasurement, devise);
+	}
+
+}
---
+++ JAVA/05_project/src/item/Item2.java
@@ -0,0 +1,55 @@
+package item;
+
+import java.util.Scanner;
+
+public class Item2 {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		Item2 item2 = new Item2();
+		int operande;
+		double number1, number2, result;
+
+		System.out.println("Entrez votre opération :");
+		System.out.print("\nchiffre 1 :");
+		number1 = sc.nextDouble();
+		System.out.println("Choisissez votre opérande :\n 1 : +\n 2 : -\n 3 : *\n 4 : /");
+		operande = sc.nextInt();
+		System.out.print("\nchiffre 2 :");
+		number2 = sc.nextDouble();
+		sc.nextLine();
+		
+		result = item2.operation(operande, number1, number2);
+		
+		System.out.println("Resultat de l'opération = "+result);
+		
+		
+	
+
+	}
+
+	public double operation(int operande, double number1, double number2) {
+
+		switch (operande) {
+		case 1: {
+			return number1 + number2;
+		}
+		case 2: {
+			return number1 - number2;
+		}
+		case 3: {
+			return number1 * number2;
+		}
+		case 4: {
+			if (number2 != 0) {
+				return number1 / number2;
+			} else {
+				throw new ArithmeticException("Division par 0 impossible");
+			}
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + operande);
+		}
+	}
+
+}
