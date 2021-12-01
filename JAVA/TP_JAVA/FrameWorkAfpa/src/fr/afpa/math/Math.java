13297882c93c89f102b2f6431e82c95982afa8d9
77011-05-07 <77011-05-07@exchange.ad.afpanet>
2021-12-01 12:55:19 +0100

    creation de l'arborescence du projet

---
+++ JAVA/TP_JAVA/FrameWorkAfpa/src/fr/afpa/math/Math.java
@@ -0,0 +1,5 @@
+package fr.afpa.math;
+
+public class Math {
+
+}
---
+++ JAVA/TP_JAVA/FrameWorkAfpa/src/fr/afpa/util/Convertisseur.java
@@ -0,0 +1,21 @@
+package fr.afpa.util;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Convertisseur {
+	Scanner sc = new Scanner(System.in);
+	JOptionPane dialog = new JOptionPane();
+	
+
+	public static void main(String[] args) {
+		boolean flag;
+		do {
+			dial
+			
+		} while (flag);
+
+	}
+
+}
