408cd545951092e816231aea6f64621bb5e937a5
77011-05-07 <77011-05-07@exchange.ad.afpanet>
2021-12-01 12:54:11 +0100

    item 3 et 4

--- JAVA/TP_JAVA/11_project/src/Item3.java
+++ JAVA/TP_JAVA/11_project/src/Item3.java
@@ -12,33 +12,34 @@ public class Item3 {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		StringBuilder sb = new StringBuilder();
-		//long noSS = 2550675113052L;
-		long noSS;
-		int key;
-		//verifieNoSS(noSS);
+		long noSS = 0;
+		int key = 0;
+	
 		System.out.println("Renseigner votre numéro de sécurité sociale");
-		String noSSString = sc.nextLine();
-		for (int i = 0; i < noSSString.length(); i++) {
-			sb.append(noSSString.replace("2A","19"));
-			sb.append(noSSString.replace("2B","18"));
-			sb.append(noSSString.replace(".",""));
-			if(noSSString.charAt(i)==60) {
-				noSS = Long.parseLong(noSSString.substring(0, i));
-				key = Integer.parseInt(noSSString.substring(i+1, i+2));
+		//String noSSString = sc.nextLine();
+		String noSSString = "2.55.06.75.113.052<79>";
+		
+			noSSString = noSSString.replace("2A","19");
+			noSSString = noSSString.replace("2B","18");
+			noSSString = noSSString.replace(".","");
+			noSSString = noSSString.replace("<","");
+			noSSString = noSSString.replace(">","");
+			
+			noSS = Long.parseLong(noSSString.substring(0, noSSString.length()-2));
+			key = Integer.parseInt(noSSString.substring(noSSString.length()-2, noSSString.length()));
+			
+			if(verifieNoSS(noSS, key)) {
+				System.out.println("OK");
+			} else {
+				System.out.println("NOK");
 			}
 		}
-		
-		//if(noSSString.length())
-		//verifieNoSS(noSS);
-	}
 	
 	static boolean verifieNoSS (long noSS, int key) {
 		int result = 97 - (int) (noSS % 97);
 		if(result==key) {
 			return true;
 		}
-		
 		return false;
 	}
 
---
+++ JAVA/TP_JAVA/11_project/src/Item4.java
@@ -0,0 +1,110 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Item4 {
+	/**
+	 * Le source ci dessous correspond au code d'un logiciel qui met en valeur les mutliples de 7 de 0 à 100. Executer ce programme avec la MV Java.
+	 * 
+	 * public class MultiplesV1{
+	 * static final int nbDiv = 7;
+	 * public static void main (String [] args){
+	 * 		System.out.println("\nAffichage des MULTIPLES de " + nbDiv + " entre 0 et 100 \n");
+	 * 
+	 * for (int x=0 x<=100;x++){
+	 * 		if(x% nbDiv == 0) System.out.println("("+ x +")\t");
+	 * 		else System.out.println(" "+ x +")\t");
+	 * 
+	 * On souhaite faire évoluer le logiciel précédent de façon à pouvoir saisir au clavier :
+	 * la valeur de nbDiv
+	 * les limites de l'ensemble des nombres à visualiser
+	 * 
+	 * La convivialité de l'application est à envisager :
+	 * 
+	 * alignement des chiffres
+	 * mode "mode page" pour des impressions de plus de 100 nombres.
+	 * traitement des réponses différentes de 0 à N à la question finale.
+	 * 
+	 * Coder ces modifications en pensant aussi à la lisibilité du programme source
+	 */
+	
+	static Scanner sc = new Scanner(System.in);
+
+	static final int NB_COLUMN = 3;
+	static final int NB_MAX_LINE_DISPLAY = 10;
+	
+	public static void main(String[] args) {
+		int NB_DIV = 7, MIN_VALUE = 0, MAX_VALUE= 100;
+		Item4 v2 = new Item4();
+		System.out.println("**************************************************************");
+		System.out.println("\tAffichage des MULTIPLES de N entre min et max");
+		System.out.println("**************************************************************");
+		
+		System.out.println("\nVeuillez renseigner N ? (Saisir un entier positif)");
+		NB_DIV = v2.returnInteger();
+		
+		System.out.println("\nVeuillez renseigner min ? (Saisir un entier positif)");
+		MIN_VALUE = v2.returnInteger();
+		
+		System.out.println("\nVeuillez renseigner max ? (Saisir un entier positif)");
+		MAX_VALUE = v2.returnInteger();
+		if(MAX_VALUE <= NB_DIV) {
+			System.out.println("\nmax est plus petit que le multiple. Veuillez renseigner une valeur de max plus grande que "+ NB_DIV);
+			MAX_VALUE = v2.returnInteger();
+		}
+		
+		v2.displayTableOfMultiple(MIN_VALUE, MAX_VALUE, NB_DIV);
+		
+		sc.close();
+	}
+	
+	public void displayTableOfMultiple(int MIN_VALUE, int MAX_VALUE, int NB_DIV) {
+		for (int i = MIN_VALUE; i <= MAX_VALUE; i++) {
+			if(i % NB_DIV == 0) {
+				System.out.print("("+ i +")\t");
+			} else {
+				System.out.print(" "+ i +"\t");
+			}
+			if(MIN_VALUE ==0) {
+				if(i % NB_MAX_LINE_DISPLAY == 0 && i % NB_COLUMN == 2) {
+					System.out.println("\n\nVeuillez appuyer sur ENTREE pour continuer l'affichage");
+					sc.nextLine();
+				} else if( i % NB_COLUMN == 2) {
+					System.out.println();
+				} 
+			}else {
+				if(i % NB_MAX_LINE_DISPLAY == 0 && i % NB_COLUMN == 0) {
+					System.out.println("\n\nVeuillez appuyer sur ENTREE pour continuer l'affichage");
+					sc.nextLine();
+				} else if( i % NB_COLUMN == 2) {
+					System.out.println();
+				} 
+			}
+			}
+		}
+	
+		public int returnInteger() {
+			int integer;
+			try {
+				integer = sc.nextInt();
+				sc.nextLine();
+				if (integer < 0) {
+					System.out.println("Veuillez saisir un entier positif :");
+					return returnInteger();
+				}
+
+			} catch (InputMismatchException e) {
+				sc.nextLine();
+				System.out.println("Mauvaise entrée, veuillez réessayer :");
+				return returnInteger();
+			}
+			return integer;
+		}
+	
+
+	
+	
+	
+}
+	
+
+
