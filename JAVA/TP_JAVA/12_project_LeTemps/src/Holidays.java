c0f33338223020618f051a272f8d27ee1783467d
77011-05-07 <77011-05-07@exchange.ad.afpanet>
2021-12-07 16:28:47 +0100

    holidays

--- JAVA/TP_JAVA/12_project_LeTemps/src/Holidays.java
+++ JAVA/TP_JAVA/12_project_LeTemps/src/Holidays.java
@@ -1,4 +1,6 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class Holidays {
 	
@@ -9,5 +11,34 @@ public class Holidays {
 	String AssomptionDay = "15/08";
 	String ToussaintDay = "01/11";
 	String ArmisticeDay = "11/11";
+	
+	String[] holidayDay = {newYearDay,  workDay,  victoryDay, BastilleDay, AssomptionDay, ToussaintDay, ArmisticeDay};
 
+
+	
+	
+	public LocalDate[] transformtoLocalDate(String year, String[] day) throws DateTimeParseException {
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate[] dayTab = new LocalDate[7];
+		
+		for (int i = 0; i < day.length; i++) {
+			dayTab[i]= LocalDate.parse(day[i]+"/"+year, format);
+		}
+		return dayTab;
+	}
+	
+	
+	public int getDayOfWeek(LocalDate[] date) {
+		int counter = 0;
+		for (int i = 0; i < date.length; i++) {
+			if(date[i].getDayOfWeek().getValue()==7 ||date[i].getDayOfWeek().getValue()==6) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+	
+	public void displayLongTextDay() {
+		
+	}
 }
--- JAVA/TP_JAVA/12_project_LeTemps/src/Regex.java
+++ JAVA/TP_JAVA/12_project_LeTemps/src/Regex.java
@@ -6,22 +6,33 @@ public class Regex {
 	public static void main(String[] args) {
 		Regex r = new Regex();
 		String[] tab = r.testRegexWithGroups("05/03");
-		//int day = Integer.parseInt(tab[0]);
-		//int month = Integer.parseInt(tab[1]);
+		int day = Integer.parseInt(tab[1]);
+		int month = Integer.parseInt(tab[2]);
 		
-		//System.out.println("day: "+day+" month: "+month);
-		System.out.println(tab[0]+" "+tab[1]);
+		System.out.println("day: "+day+" month: "+month);
+
 	
 	}
-	
+	/**
+	 * 
+	 * @param date une date sous le format dd/MM
+	 * @return tableau de String dans lequel à l'index 0 nous renseignons le nombre de jours et 
+	 * à l'index 1 le nombre de mois.
+	 * @throws NullPointerException si la date ne correspond pas au format dd/MM retourne null plutot qu'un tableau vide.
+	 */
 	public String[] testRegexWithGroups(String date) throws NullPointerException {
-		Pattern pattern = Pattern.compile("(\\d{1,2})/(\\d{1,2})");
-		// (?<day>)(\\d{1,2})/(?<month>)(\\d{1,2})
+		Pattern pattern = Pattern.compile("(<?day>\\d{1,2})>\\/(<?month>\\d{1,2})");
+		// (\\d{1,2})/(\\d{1,2})
 		Matcher matcher = pattern.matcher(date);
 		String[] dateTab = new String[2];
 		if(matcher.find()) {
-			dateTab[0] = matcher.group(1);
-			dateTab[1] = matcher.group(2);
+			// ATTENTION matcher.group(0) = TOTALITE DU MATCH !!!
+			// dateTab[0] = matcher.group(1);
+			// dateTab[1] = matcher.group(2);
+			
+			dateTab[0] = matcher.group("day");
+			dateTab[1] = matcher.group("month");
+			
 		}
 		if(dateTab[0]!=null && dateTab[1]!=null) {
 			return dateTab;
