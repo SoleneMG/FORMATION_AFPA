cf4c18bf786cd8fb4471ab6c2a7a26e5ba607f61
77011-05-07 <77011-05-07@exchange.ad.afpanet>
2022-01-26 17:25:17 +0100

    html

---
+++ HTML/JeuPlusOuMoins/public/index.js
@@ -0,0 +1,29 @@
+const numberField = document.querySelector("#number");
+const lastNumberField = document.querySelector("#lastNumber");
+const trialNumberField = document.querySelector("#trial");
+const adviceField = document.querySelector("#advice");
+const commentField = document.querySelector("#comment");
+
+const number = document.querySelector("#number").value;
+const lastNumber = document.querySelector("#lastNumber").value;
+const trialNumber = document.querySelector("#trial").value;
+const advice = document.querySelector("#advice").value;
+const comment = document.querySelector("#comment").value;
+
+const max = 500;
+const min = 1;
+const mysteryNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(mysteryNumber);
+
+function isOk(){
+    if(number == mysteryNumber){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function display(){
+
+}
